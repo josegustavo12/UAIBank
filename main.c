f677e71ddb17d6ae34b53f25a1b6d5a8dd9c8b76
josegustavo12 <josegustavovictor@gmail.com>
2024-06-07 11:40:12 -0300

    criação da intersecaounica e intersecaovarios

--- main.c
+++ main.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void insercaounica(char nome, int idade, int saldo_atual){
+typedef struct {
+    int id;
+    char nome[100];
+    int idade;
+    int saldo_atual;
+} User;
+
+User* users = NULL;
+int user_count = 0;
+int next_id = 1;
+
+void insercaounica(char* nome, int idade, int saldo_atual) {
+
+    users = realloc(users, (user_count + 1) * sizeof(User));
+    users[user_count].id = next_id++;
+    strcpy(users[user_count].nome, nome);
+    users[user_count].idade = idade;
+    users[user_count].saldo_atual = saldo_atual;
+    user_count++;
 }
 
-void insercaovarios(int qnt_usuarios, char nome, int idade, int saldo_atual){
+void insercaovarios(int qnt_usuarios, char* nomes[], int idades[], int saldos[]) {
+    for (int i = 0; i < qnt_usuarios; i++) {
+        insercaounica(nomes[i], idades[i], saldos[i]);
+    }
 }
 
-void buscarid(int id){
+User* buscarid(int id) {
 }
 
-void transferencia(int id_origem, int id_destino, int quantia){
+void transferencia(int id_origem, int id_destino, int quantia) {
 }
 
-void remocao(int id){
+void remocao(int id) {
 }
 
-int main(){
-
+int main() {
 
     return 0;
 }
