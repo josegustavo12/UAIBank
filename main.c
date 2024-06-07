1706001d2fcf110316fa63921cf4824c8fa592c5
josegustavo12 <josegustavovictor@gmail.com>
2024-06-06 21:27:16 -0300

    criação da main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void insercaounica(char nome, int idade, int saldo_atual){
+}
+
+void insercaovarios(int qnt_usuarios, char nome, int idade, int saldo_atual){
+}
+
+void buscarid(int id){
+}
+
+void transferencia(int id_origem, int id_destino, int quantia){
+}
+
+void remocao(int id){
+}
+
+int main(){
+
+
+    return 0;
+}
