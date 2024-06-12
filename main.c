3a614253188601cc6d3a31d6de8f063289689791
pedrob21 <154927859+pedrob21@users.noreply.github.com>
2024-06-11 22:30:12 -0300

    Menu feito

--- main.c
+++ main.c
@@ -18,6 +18,40 @@ int idorigem, iddestino;
 float quantia;
 };
 
+void menu(int opc){
+    printf("\n(1) Inserção de um novo usuário.\n(2) Inserção de varios usuários.\n(3) Busca de usuário por id.\n(4) Transferência entre usuários.\n(5)Remoção de um usuário.\n(6) Sair.");
+    scanf("%d", &opc);
+    switch (opc) {
+        case 1:
+            printf("Opção 1 selecionada: Inserção de um novo usuário.\n");
+            insercaounica();
+            break;
+        case 2:
+            printf("Opção 2 selecionada: Inserção de varios usuários.\n");
+            insercaovarios();
+            break;
+        case 3:
+            printf("Opção 3 selecionada: Busca de usuário por id.\n");
+            buscarid();
+            break;
+        case 4:
+            printf("Opção 4 selecionada: Transferência entre usuários.\n");
+            transferencia();
+            break;
+        case 5:
+            printf("Opção 5 selecionada: Remoção de um usuário.\n");
+            remocao();
+            break;
+        case 6:
+            printf("Obrigado por utilizar o UaiBank!\nSaindo...");
+            break;
+        default:
+            printf("Por favor, insira uma opção válida.\n");
+            menu();
+            break;
+    }
+}
+
 void insercaounica(char* nome, int idade, int saldo_atual) {
 
     users = realloc(users, (user_count + 1) * sizeof(User));
@@ -67,28 +101,20 @@ void remocao(int id) {
 }
 
 int main() {
-    int opc;
-    do{
-        printf ("Digite o Id de destino:\n");
-    scanf ("%d", &iddestino);
-    if (iddestino == NULL){
-        printf ("Id não existente. Digite um Id válido");
+    int redo;
+    printf("Seja bem-vindo(a) ao UaiBank!\nPor favor, selecione a opção desejada:");
+    menu();
+    while(redo!=1 || redo!=2){
+        printf("Deseja realizar alguma nova ação?\n(1) Sim.\n(2) Não.\n");
+        scanf("%d", &redo");
+        printf("Por favor, insira uma opção válida");
     }
-    else {
-        printf ("Digite a quantia a ser transferida:\n");
-        scanf ("%f", &quantia);
-        if (saldo[Idorigem]<quantia){
-            printf ("Não foi possível realizar a transferencia. Saldo insuficiente.")
-        }
-        else {
-            saldo[IdOrigem]= saldoatual - quantia;
-            saldo [IdDestino] = saldoatual + quantia;
-            printf ("Transferencia realizada com sucesso.")
-            printf ("Deseja realizar outra transferencia? 1(SIM) 2(NAO)")
-        }
-     while (opc==1)
-           
-        }
+    if(redo==1){
+        menu();
+    }
+    else if(redo==2){
+        menu(6);
+    }
     }
     return 0;
 }
