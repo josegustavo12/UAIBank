83e0e12fc0714780039ca63cc37b84ee24168ae9
José Gustavo Victor <145945740+josegustavo12@users.noreply.github.com>
2024-06-20 22:15:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -21,42 +21,43 @@ float quantia;
 void menu(int opc){
     switch (opc) {
         case 1:
-            printf("Opção 1 selecionada: Inserção de um novo usuário.\n");
+            printf("\nOpção 1 selecionada: Inserção de um novo usuário.\n");
             insercaounica();
             break;
         case 2:
-            printf("Opção 2 selecionada: Inserção de varios usuários.\n");
+            printf("\nOpção 2 selecionada: Inserção de varios usuários.\n");
             insercaovarios();
             break;
         case 3:
-            printf("Opção 3 selecionada: Busca de usuário por id.\n");
+            printf("\nOpção 3 selecionada: Busca de usuário por id.\n");
             buscarid();
             break;
-        case 4:
-            printf("Opção 4 selecionada: Transferência entre usuários.\n");
-            transferencia();
-            break;
         case 5:
-            printf("Opção 5 selecionada: Remoção de um usuário.\n");
+            printf("\nOpção 5 selecionada: Remoção de um usuário.\n");
             remocao();
             break;
         case 6:
-            printf("Obrigado por utilizar o UaiBank!\nSaindo...");
+            printf("\nSaindo...");
             break;
         default:
-            printf("Opção inválida.\n");
+            printf("\nOpção inválida.\n");
             break;
     }
 }
 
-void insercaounica(char* nome, int idade, int saldo_atual) {
-
+void insercaounica() {
+    char nome[100];
+    int idade;
+    float saldo_atual;
+    printf("\nNome, Idade, Saldo atual:\n");
+    scanf("%s, %d, %f", &nome, &idade, &saldo_atual);
     users = realloc(users, (user_count + 1) * sizeof(User));
     users[user_count].id = next_id++;
     strcpy(users[user_count].nome, nome);
     users[user_count].idade = idade;
     users[user_count].saldo_atual = saldo_atual;
     user_count++;
+    printf("\n======================Inserção realizada com sucesso======================\n");
 }
 
 void insercaovarios(int qnt_usuarios, char* nomes[], int idades[], int saldos[]) {
@@ -79,46 +80,22 @@ void buscarid(User *users) {
     printf("Usuário não encontrado\n");
 }
 
-void transferencia(int id_origem, int id_destino, int quantia) {
-     int opc;
-    do{
-        printf ("Digite o Id de destino:\n");
-    scanf ("%d", &iddestino);
-    if (iddestino == NULL){
-        printf ("Id não existente. Digite um Id válido");
-    }
-    else {
-        printf ("Digite a quantia a ser transferida:\n");
-        scanf ("%f", &quantia);
-        if (saldo[Idorigem]<quantia){
-            printf ("Não foi possível realizar a transferencia. Saldo insuficiente.")
-        }
-        else {
-            saldo[IdOrigem]= saldoatual - quantia;
-            saldo [IdDestino] = saldoatual + quantia;
-            printf ("Transferencia realizada com sucesso.")
-            printf ("Deseja realizar outra transferencia? 1(SIM) 2(NAO)")
-        }
-     while (opc==1)
-           
-        }
-    }
-}
 
 void remocao(int id) {
 }
 
 int main() {
     int redo = 1;
+    int opc;
 
     do{printf("Seja bem-vindo(a) ao UaiBank!\n");
         printf("Por favor, selecione a opção desejada:");
-        printf("\n(1) Inserção de um novo usuário.\n(2) Inserção de varios usuários.\n(3) Busca de usuário por id.\n(4) Transferência entre usuários.\n(5)Remoção de um usuário.\n(6) Sair.");
+        printf("\n(1) Inserção de um novo usuário.\n(2) Inserção de varios usuários.\n(3) Busca de usuário por id.\n(4) Transferência entre usuários.\n(5)Remoção de um usuário.\n(6) Sair.\n\n");
         scanf("%d", &opc);
         menu(opc);
-        printf("Deseja realizar uma nova ação?\n(1) Sim\n(2) Não");
+        printf("Deseja realizar uma nova ação?\n(1) Sim (2) Não\n\n");
         scanf("%d", &redo);
     }while(redo==1);
-    printf("Obrigado por utilizar o UaiBank!");
+    printf("\nObrigado por utilizar o UaiBank!!!");
     return 0;
 }
